ee69f27a441395952b34e08c7c0ab8c70ad3a746
Gianluca <gbelardelli@gmail.com>
2025-05-17 13:51:59 +0200

    #simpleblit - blitter copying

--- simpleblit/blitter.s
+++ simpleblit/blitter.s
@@ -10,6 +10,7 @@
 ;********************************************************
     XDEF        WaitBlitter
     XDEF        MemClear
+    XDEF        MemCopy
 
     SECTION     code_section,CODE
 
@@ -37,8 +38,33 @@ MemClear:
 
     move.w  #DEST,BLTCON0(a5)   ; Enable only DEST DMA
                                 ; MINTERMS = $0 -> D
-    move.w  #0,BLTDMOD          ; Modulo 0
-    move.w  #0,BLTCON1          ; BLTCON1 = 0
+    move.w  #0,BLTDMOD(a5)      ; Modulo 0
+    move.w  #0,BLTCON1(a5)      ; BLTCON1 = 0
+    move.l  a1,BLTDPT(a5)       ; Dest address
+    move.w  d1,BLTSIZE(a5)      ; Size in words
+
+    bsr.s   WaitBlitter
+
+    rts
+
+
+;********************************************************
+; MemCopy - Simple A=D copy
+; a0 -> src addr
+; a1 -> dest addr
+; d1 -> size in words (max 64)
+;********************************************************
+MemCopy:
+    bsr.s   WaitBlitter
+
+    move.w  #ABC|ABNC|ANBC|ANBNC|SRCA|DEST,BLTCON0(a5)  ; Enable SRCA and DEST DMA
+                                                        ; MINTERMS = $F0 -> A=D
+    move.w  #0,BLTAMOD(a5)      ; Channel A Modulo 0
+    move.w  #32,BLTDMOD(a5)     ; Channel D Modulo 0
+    move.w  #$FFFF,BLTAFWM(a5)  ; Channel A Mask
+    move.w  #$FFFF,BLTALWM(a5)  ; Channel A Mask
+    move.w  #0,BLTCON1(a5)      ; BLTCON1 = 0
+    move.l  a0,BLTAPT(a5)       ; Src address
     move.l  a1,BLTDPT(a5)       ; Dest address
     move.w  d1,BLTSIZE(a5)      ; Size in words
 
--- simpleblit/main.s
+++ simpleblit/main.s
@@ -23,18 +23,42 @@ _main:
     move.w      #$FFFF,(a1)+
     dbra        d7,.loop
 
-    lea         screen,a1
-    add         #160,a1
-    move.w      #83*64+60,d1        ; 249 righe/3 = 83 + 60 word(3 righe)
-                                    ; = 83*64+60
-    jsr         MemClear
+;     lea         screen,a1
+;     add         #160,a1
+;     move.w      #83*64+60,d1        ; 249 righe/3 = 83 + 60 word(3 righe)
+;                                     ; = 83*64+60
+;     jsr         MemClear
 
-.mainloop:
+; .mainloop:
+;     btst        #6,$BFE001
+;     bne.s       .mainloop
+
+    move.w      #64*64+4,d1
+    move.l      #4,d6
+    move.l      #40,d5
+.loop0:
+    lea         tile,a0
+    lea         screen,a1
+    sub.l       #8,d5
+    add.l       d5,a1
+    move.l      #2,d7
+.loop1:
+    jsr         MemCopy
+    add         #512,a0
+    add.l       #10240,a1
+    dbra        d7,.loop1
+    dbra        d6,.loop0
+
+.mainloop2:
     btst        #6,$BFE001
-    bne.s       .mainloop
+    bne.s       .mainloop2
 
     jsr         ReleaseSystem
 
     rts
+
+    SECTION graphic,DATA_C
+tile:
+    INCBIN      "gfx/tile.raw"
 
 	END
